44cc5cc4f14e15473f71bb0f240e0e2583fad6d4
asengelhart <asengelhart@yahoo.com>
2020-07-09 18:39:37 -0600

    reminder mailer and view

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,2 +1,6 @@
 class ReminderMailer < ApplicationMailer
+  def reminder_mail(member_id)
+    @member = Member.find_by(id: member_id)
+    mail(to: @member.email, subject: ENV["REMINDER_SUBJECT"] || "Time to renew your Food Hub membership!")
+  end
 end
