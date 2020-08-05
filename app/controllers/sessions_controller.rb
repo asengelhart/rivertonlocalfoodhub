d2d4a5c2255d3e887670e5b07658338b836c628c
asengelhart <asengelhart@yahoo.com>
2020-08-04 18:38:39 -0600

    create session controller w/ routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      flash.now[:alert] = "Incorrect username or password."
+      render :new
+    end
+  end
+
+  def login_params
+    params.require(:user).permit(:name, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :members
   resources :users
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
